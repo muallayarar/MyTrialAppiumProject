fe12b89b8526521da65301ae4a7defa4279f5cc2
Mualla Yarar <muallayarar@gmail.com>
2023-04-19 23:41:29 +0300

    Initial commit

---
+++ src/test/java/Appium/Appium01.java
@@ -0,0 +1,38 @@
+package Appium;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium01 {
+/*
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        //capabilities.setCapability("platformName", "Android");
+        //capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android");
+        String filePath = System.getProperty("user.dir");
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "90eed57a");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        //capabilities.setCapability(MobileCapabilityType.UDID,"90eed57a");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\muall\\IdeaProjects\\MyTrialAppiumProject\\src\\Apps\\gestureTool.apk");
+        //capabilities.setCapability("appPackage", "com.miui.calculator");
+        //capabilities.setCapability("appActivity", "com.miui.calculator.cal.CalculatorActivity");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "6000");
+
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+ */
+}
+//Pixel 2 API 33
